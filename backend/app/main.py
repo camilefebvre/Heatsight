d10a6a9526a846c76a19b197de4b46a980264d4a
camille lefebvre <camille@lefebvre.com>
2026-03-06 01:16:35 +0100

    fix CORS allow_origin_regex

--- backend/app/main.py
+++ backend/app/main.py
@@ -65,7 +65,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://heatsight.vercel.app"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://heatsight.vercel.app",
+    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
